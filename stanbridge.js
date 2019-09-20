c892ebe66b2136c21a137e61229216680da939b7
Ashley Stanbridge <ashley_stanbridge@yahoo.com>
2019-09-20 17:21:20 -0500

    Fixed error in calculations. Formatted code to have $ once value is found

--- stanbridge.js
+++ stanbridge.js
@@ -8,25 +8,25 @@ const calcTip = (total, rate) => {
 }
 
 const calcTax = (total) => {
-    return total * (5.5/100);
+    return total * (5.50/100);
 }
 
 
 document.querySelector("#calc-button").addEventListener("click", () => {
+    //Reading in total and rate value
     const total = parseFloat(document.querySelector("#total").value);
     const rate = parseFloat(document.querySelector("#rate").value);
-    const tip = parseFloat(document.querySelector("#tip").value);
-    const tax = 5.50;
-
+    
+    //Calculating tip, tax, and grand total
     const tipTotal = calcTip(total, rate);
     const taxTotal = calcTax(total);
     const result = calcTotal(total, tipTotal, taxTotal);
 
+    //Displaying results
     document.querySelector("#rate").innerHTML = rate;
-    document.querySelector("#tip").innerHTML = tipTotal;
-    document.querySelector("#tax").innerHTML = tax;
-
-    document.querySelector("#grand-total").innerHTML = result;
+    document.querySelector("#tip").innerHTML = "$" + tipTotal.toFixed(2);
+    document.querySelector("#tax").innerHTML = "$" + taxTotal.toFixed(2);
+    document.querySelector("#grand-total").innerHTML = "$" + result.toFixed(2);
     
 });
     
